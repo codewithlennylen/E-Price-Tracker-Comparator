8bb31135d02c1406426bf4790d666325dec6efe8
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-03-12 23:06:43 +0300

    Update main.py
    I am still having trouble with scraping Amazon. This shit is crazy frustrating. Fudge!!!!
    
    Might have to switch over to scrapy!

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 
-urlLink = 'https://www.amazon.com/s?k=gtx+1650&ref=nb_sb_noss'
+# This works, with or without the query after ?. I also had to select the item after searching
+urlLink = 'https://www.amazon.com/ZOTAC-GeForce-128-Bit-Graphics-ZT-T16500F-10L/dp/B07QF1H9YR/ref=sr_1_1'
+urlLink0 = 'https://www.amazon.com/ZOTAC-GeForce-128-Bit-Graphics-ZT-T16500F-10L/dp/B07QF1H9YR/ref=sr_1_1?dchild=1&keywords=gtx+1650&qid=1584041832&sr=8-1'
+
 # urlLink1 = 'https://pythonprogramming.net/parsememcparseface/'
 # urlLink2 = 'https://www.amazon.com/ZOTAC-GeForce-128-Bit-Graphics-ZT-T16500F-10L/dp/B07QF1H9YR/ref=sxin_0_ac_d_rm?ac_md=0-0-Z3R4IDE2NTA%3D-ac_d_rm&crid=VIKK28PMLWYE&cv_ct_cx=gtx+1650&keywords=gtx+1650&pd_rd_i=B07QF1H9YR&pd_rd_r=2193b6dc-a98d-490a-a2b1-ed061779b818&pd_rd_w=1A0As&pd_rd_wg=apW3M&pf_rd_p=ec111f65-4a46-499c-be78-f47997212bd0&pf_rd_r=1ARPC3YJKGRHDDQ75R83&psc=1&qid=1583787604&sprefix=gtx%2Caps%2C864'
 # urlLink3 = 'https://www.amazon.com/s?k=gtx+1650&i=electronics&ref=nb_sb_noss'
-# urlLink4 = 'https://www.amazon.com/s?k=gtx+1650&i=electronics&rh=p_72%3A1248879011%2Cp_36%3A1253506011&dc&qid=1583787867&rnid=386442011&ref=sr_nr_p_36_4'
+# urlLink4 = 'https://www.amazon.com/ZOTAC-GeForce-128-Bit-Graphics-ZT-T16500F-10L/dp/B07QF1H9YR/ref=sr_1_1?dchild=1&keywords=gtx+1650&qid=1584041832&sr=8-1'
 
-page = requests.get(urlLink, headers=headers)
-# print(page)
+page = requests.get(urlLink0, headers=headers)
+print(page) # Get the response code
 
+# soup = BeautifulSoup(page.content, features='lxml') # Returns an Array
+soup = BeautifulSoup(page.content, 'html.parser') # Still works
+# print(soup.prettify())
 
-soup = BeautifulSoup(page.content, 'html.parser')
+# product_title = soup.select('#productTitle')[0].get_text().strip()
+# print(product_title)
+title = soup.find(id="productTitle").get_text()
+print(title)
 
-# cards = soup.select("#a-section a-spacing-none")[0].get_text().strip() # first element of array, strip whitespaces.
-
-# contentCards = soup.findAll('div',{'class':'a-section a-spacing-none'})
-# contentCards = soup.find_all('h2')
-results = soup.find_all('h2')
-print(results)
 
 # f = open('results.html','w')
 # for i in results:
