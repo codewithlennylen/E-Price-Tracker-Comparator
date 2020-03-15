369870cb1a51a53f3f30581dceac05a7b5e67a8e
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-03-15 23:42:53 +0300

    Scrapy + CSS Selector Gadget Works like a Charm :+1: :100:
    One preliminary test has proved that Scrapy is way powerful and that the crawler for the application will be implemented using Scrapy.

--- amazoon/amazoon/settings.py
+++ amazoon/amazoon/settings.py
@@ -17,6 +17,7 @@ NEWSPIDER_MODULE = 'amazoon.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'amazoon (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- amazoon/amazoon/spiders/amazoon_spider.py
+++ amazoon/amazoon/spiders/amazoon_spider.py
@@ -5,10 +5,20 @@ from ..items import AmazoonItem
 class AmazoonSpiderSpider(scrapy.Spider):
     name = 'amazoon_spider'
     start_urls = [
-    				'https://www.amazon.com/ZOTAC-GeForce-128-Bit-Graphics-ZT-T16500F-10L/dp/B07QF1H9YR/ref=sxin_0_ac_d_rm?ac_md=0-0-Z3R4IDE2NTA%3D-ac_d_rm&cv_ct_cx=gtx+1650&dchild=1&keywords=gtx+1650&pd_rd_i=B07QF1H9YR&pd_rd_r=e02dc07c-ba53-4b0f-b567-079c046a49b4&pd_rd_w=UA9qw&pd_rd_wg=IZlq2&pf_rd_p=ec111f65-4a46-499c-be78-f47997212bd0&pf_rd_r=DAVQRP12X837VEDJWBVD&psc=1&qid=1584126895'
+    				'https://www.amazon.com/s?k=gtx+1650&i=electronics&ref=nb_sb_noss'
     			]
 
     def parse(self, response):
         items = AmazoonItem()
 
-        product_name = response.css()
+        product_name = response.css('.a-color-base.a-text-normal::text').extract()
+        product_author = response.css('.a-link-normal.a-text-bold').css('::text').extract()
+        product_price = response.css('.a-price-whole').css('::text').extract()
+        product_imagelink = response.css('.s-image::attr(src)').extract()
+
+        items['product_name'] = product_name
+        items['product_author'] = product_author
+        items['product_price'] = product_price
+        items['product_imagelink'] = product_imagelink
+
+        yield items
