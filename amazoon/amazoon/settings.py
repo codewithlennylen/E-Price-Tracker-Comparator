c7b10a18aa320b3574a68bd9b4dbda24d1423cfa
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-03-28 05:28:56 +0300

    Scrapy-user-agents
    Rotating user agents to avoid the bot from being blocked by the websites it visits.

--- amazoon/amazoon/settings.py
+++ amazoon/amazoon/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'amazoon.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'amazoon (+http://www.yourdomain.com)'
-USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+# USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -50,6 +50,11 @@ ROBOTSTXT_OBEY = True
 #SPIDER_MIDDLEWARES = {
 #    'amazoon.middlewares.AmazoonSpiderMiddleware': 543,
 #}
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
